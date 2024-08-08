052e8585adef45fcd700a70c70c757c67d08d22c
es-sayers <esayers.cs@gmail.com>
2024-08-08 15:56:58 -0400

    Change Tag::$children to Collection::class

--- tests/Tags/TagTest.php
+++ tests/Tags/TagTest.php
@@ -1,6 +1,5 @@
 <?php
 
-namespace Esayers\Tests\Tags;
 namespace Esayers\Html\Tests\Tags;
 
 use Esayers\Html\Tags\Tag;
@@ -11,6 +10,33 @@ use PHPUnit\Framework\TestCase;
 #[CoversClass('Tag')]
 class TagTest extends TestCase
 {
+    #[Test]
+    public function testConstructAndGet()
+    {
+        $tag = new Tag('div');
+        $this->assertEquals([], $tag->children()->toArray());
+    }
+
+    #[Test]
+    public function testConstructWithChildren()
+    {
+        $child = new Tag('p');
+        $tag = new Tag('div', [$child]);
+
+        $this->assertEquals([$child], $tag->children()->toArray());
+    }
+
+    #[Test]
+    public function testConstructWithAttributes()
+    {
+        $attributes = [
+            'class' => ['class1', 'class2'],
+            'id' => '2'
+        ];
+        $tag = new Tag('div', [], $attributes);
+        $this->assertEquals($attributes, $tag->getAttributes());
+    }
+
     #[Test]
     public function testRender()
     {
@@ -26,60 +52,10 @@ class TagTest extends TestCase
     }
 
     #[Test]
-    public function testGetChildren()
-    {
-        $tag = new Tag('div');
-
-        $this->assertEquals([], $tag->getChildren());
-    }
-
-    #[Test]
-    public function testChildrenInConstructor()
-    {
-        $child = new Tag('p');
-        $tag = new Tag('div', [$child]);
-
-        $this->assertEquals([$child], $tag->getChildren());
-    }
-
-    #[Test]
-    public function testAddChild()
-    {
-        $child1 = new Tag('p');
-        $child2 = new Tag('p');
-        $tag = new Tag('div');
-        $tag->child($child1);
-        $tag->child($child2);
-
-        $this->assertEquals([$child1, $child2], $tag->getChildren());
-    }
-
-    #[Test]
-    public function testAddChildren()
-    {
-        $child1 = new Tag('p');
-        $child2 = new Tag('p');
-        $tag = new Tag('div');
-        $tag->children([$child1, $child2]);
-
-        $this->assertEquals([$child1, $child2], $tag->getChildren());
-    }
-
-    #[Test]
-    public function testConstructWithAttributes()
-    {
-        $attributes = [
-            'class' => ['class1', 'class2'],
-            'id' => '2'
-        ];
-        $tag = new Tag('div', [], $attributes);
-        $this->assertEquals($attributes, $tag->getAttributes());
-    }
-
-    #[Test]
     public function testRenderWithAttributes()
     {
         $tag = new Tag('div', [], ['id' => '2']);
+
         $this->assertEquals('<div id="2"></div>', $tag->render());
     }
 }
