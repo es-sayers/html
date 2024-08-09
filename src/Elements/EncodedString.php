9edc8ad2e03c44eb9db76649255c12e4543dbff0
es-sayers <esayers.cs@gmail.com>
2024-08-09 15:49:14 -0400

    Add EncodedString class

---
+++ src/Elements/EncodedString.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Esayers\Html\Elements;
+
+use Esayers\Html\AbstractEncodedRenderable;
+
+class EncodedString extends AbstractEncodedRenderable
+{
+    /**
+     * Raw text to render
+     * @var string
+     */
+    protected string $string;
+
+    /**
+     * @param string $string Unencoded string
+     */
+    public function __construct(string $string)
+    {
+        $this->string = $string;
+    }
+
+    /**
+     * Static constructor alias
+     *
+     * @param string $string Unencoded string
+     * @return EncodedString
+     */
+    public static function new(string $string): EncodedString
+    {
+        return new static($string);
+    }
+
+    /**
+     * Shortcut to render encoded string
+     *
+     * @param string $string Unencoded string
+     * @return string Encoded string
+     */
+    public static function from(string $string): string
+    {
+        return (new static($string))->render();
+    }
+
+    /**
+     * @inheritDoc
+     */
+    protected function renderString(): string
+    {
+        return $this->string;
+    }
+}
