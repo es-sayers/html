f299d6996ed400198bf693f145c141816a120c3c
es-sayers <esayers.cs@gmail.com>
2024-08-09 15:47:15 -0400

    Create typed collections

---
+++ src/Elements/TagCollection.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Esayers\Html\Elements;
+
+use Esayers\Html\AbstractRenderable;
+use Esayers\Html\Elements\Tag;
+use Esayers\Html\TypedCollection;
+
+class TagCollection extends AbstractRenderable
+{
+    /**
+     * @var TypedCollection
+     */
+    private TypedCollection $tags;
+
+    /**
+     * @param Tag[] $tags
+     */
+    public function __construct(array $tags = [])
+    {
+        $this->tags = new TypedCollection(Tag::class, $tags);
+    }
+
+    protected function renderString(): string
+    {
+        $renderString = '';
+        foreach ($this->tags as $tag) {
+            $renderString .= $tag->render();
+        }
+        return $renderString;
+    }
+}
